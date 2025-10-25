4501718a0a2dc18a1bcc3ac7ad61419c23bc10f0
Alireza-F1 <alirzamhp79@gmail.com>
2025-10-25 18:40:19 +0330

    Vite.config has been edited

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
     vue(),
     tailwindcss(),
   ],
+  base: '/To-Do-list/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
